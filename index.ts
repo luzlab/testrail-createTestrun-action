4fde02fce1d37e25720aabdcfadebeb5fd4855f9
Quiñónez, Carlo J <carlo.quinonez@thermofisher.com>
2021-07-23 10:53:32 -0700

    Add testrail user parameter and call testrail api

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import Testrail from 'testrail-api';
 
 const { getInput, setOutput, setFailed, debug } = core;
 const { context, getOctokit } = github;
@@ -10,20 +11,24 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(context.payload, undefined, 2)
   debug(`The event payload: ${payload}`);
-  
-  const testrailSuite = getInput('testrail_suite');
-  const testrailProject = getInput('testrail_project');
-  const testrailURL = getInput('testrail_URL');
 
-  debug(`testrailSuite ${testrailSuite}!`);
-  debug(`testrailProject ${testrailProject}!`);
-  debug(`testrailURL ${testrailURL}!`);
-  
+  const host = getInput('testrail_URL');
+  const password = getInput('testrail_token')
+  const user = getInput('testrail_user')
+  const testrail = new Testrail({ host, password, user });
+
+  const testrailSuite = parseInt(getInput('testrail_suite'));
+  const testrailProject = parseInt(getInput('testrail_project'));
+
+  const project = testrail.getProject(testrailProject)
+  console.log(project);
+
+  const suite = testrail.getSuite(testrailSuite)
+  console.log(suite);
+
   const time = new Date().toTimeString();
   setOutput("testrun_URL", time);
   setOutput("testrun_ID", Math.random()*100);
-  
-
 } catch (error) {
   setFailed(error.message);
 }
