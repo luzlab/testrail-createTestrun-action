e2c1066f5fd1479241ade0ad6a678910d71f96cf
Quiñónez, Carlo J <carlo.quinonez@thermofisher.com>
2021-07-23 15:27:47 -0700

    Create testrun

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import Testrail from 'testrail-api';
-import { PullRequestOpenedEvent }  from '@octokit/webhooks-types'
+import Testrail, { INewTestRun } from 'testrail-api';
+import { PullRequestOpenedEvent } from '@octokit/webhooks-types';
+import { stringify } from 'yaml';
 
 const { getInput, setOutput, setFailed, debug } = core;
 const { context, getOctokit } = github;
@@ -9,7 +10,19 @@ const { context, getOctokit } = github;
 run();
 async function run() {
   try {
-    const octokit = getOctokit(getInput('github_token'));
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const {
+      number: prNumber,
+      pull_request,
+      repository,
+    } = context.payload as PullRequestOpenedEvent;
+    const {
+      owner: { login },
+      name,
+    } = repository;
+    const { title: prTitle } = pull_request;
+
+    const octokit = getOctokit(getInput('github_token')).rest;
 
     const host = getInput('testrail_URL');
     const password = getInput('testrail_token');
@@ -25,16 +38,48 @@ async function run() {
     const suite = (await testrail.getSuite(testrailSuite)).body;
     console.log(suite);
 
+    const pullrequestComment = `This comment was auto-generated and contains information used by the TestRail/GitHub integration\nDO NOT EDIT.`;
+    const pullrequestData = {
+      testrailHost: host,
+      testProject: project.name,
+      testSuite: suite.name,
+      repoOwner: login,
+      repoName: name,
+      pullrequest: prNumber,
+      pullrequestTitle: prTitle,
+    };
+    const body = `${pullrequestComment}\n...\n${stringify(pullrequestData)}`;
+    console.log(body);
+
+    octokit.issues.createComment()
+
+    const testrunDescription = `This testrun was auto-generated for a GitHub pull request. Please add test cases and run as needed. Click the "Push Results" button to send the test results to Github.\n\n##### DO NOT EDIT DESCRIPTION BELOW THIS LINE ######`;
+    const testrunData = {
+      testrailHost: host,
+      testProject: project.name,
+      testSuite: suite.name,
+      repoOwner: login,
+      repoName: name,
+      pullrequest: prNumber,
+      pullrequestTitle: prTitle,
+    };
+    const description = `${pullrequestComment}\n...\n${stringify(pullrequestData)}`;
+
+    const testrunRequest = {
+      suite_id: suite.id,
+      name: `PR${prNumber}: ${prTitle}`,
+      include_all: false,
+      case_ids: [],
+      refs: [prNumber],
+      description
+    };
+
+    const testrun = await testrail.addRun(project.id, testrunRequest);
+    console.log(testrun);
+    
     const time = new Date().toTimeString();
     setOutput('testrun_URL', time);
     setOutput('testrun_ID', Math.random() * 100);
-
-    console.log(context.payload);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const pullrequest = context.payload.pull_request as PullRequestOpenedEvent;
-    
-    console.log(pullrequest)
-
   } catch (error) {
     setFailed(error.message);
   }
